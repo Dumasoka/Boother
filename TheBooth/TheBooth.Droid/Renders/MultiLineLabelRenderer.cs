53a47d0a6b40fc1c37145a97f8ebecef40823663
Duma Masoka <duma@home-grown.co.za>
2017-07-12 11:35:45 +0200

    linebreaks and new icons

---
+++ TheBooth/TheBooth.Droid/Renders/MultiLineLabelRenderer.cs
@@ -0,0 +1,20 @@
+﻿using TheBooth.Common;
+using TheBooth.Droid.Renders;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(MultiLineLabel), typeof(MultiLineLabelRenderer))]
+namespace TheBooth.Droid.Renders
+{
+    public class MultiLineLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            var baseLabel = (MultiLineLabel)this.Element;
+
+            Control.SetLines(baseLabel.Lines);
+        }
+    }
+}
---
+++ TheBooth/TheBooth/Common/MultiLineLabel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TheBooth.Common
+{
+    public class MultiLineLabel : Label
+    {
+        // Default number of lines is 1 so MultiLineLabel behaves like a standard Label if Lines is not set
+        public int Lines = 3;
+    }
+}
